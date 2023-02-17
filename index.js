9b8bc9c015392be097a65dec140637394ba4bd77
Reese-haids <reese.yes@gmail.com>
2023-02-17 19:29:21 +0800

    2nd commit

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // IN PROGRESS!!!!!
+
+const hexInput = document.getElementById("hexInput");
+const inputColor = document.getElementById("inputColor");
+
+hexInput.addEventListener("keyup", () => {
+  const hex = hexInput.value;
+  if (!isValidHex(hex)) return;
+
+  const strippedHex = hex.replace("#", "");
+
+  inputColor.style.backgroundColor = "#" + strippedHex;
+});
+
+const isValidHex = (hex) => {
+  if (!hex) return false;
+
+  const strippedHex = hex.replace("#", "");
+  return strippedHex.length === 3 || strippedHex.length === 6;
+};
+
+//Create a function to convert Hex to RGB
+//this should work with 3 or 6 character hex values
+//Hint - use parseInt("", 16) to convert a hex value to a decimal value
+//should return an object with 3 properties - r,g, and b
+//Test your function with a few different use cases
+
+const convertHexToRGB = (hex) => {
+  if (!isValidHex(hex)) return null;
+
+  let strippedHex = hex.replace("#", "");
+
+  if (strippedHex.length === 3) {
+    strippedHex =
+      strippedHex[0] +
+      strippedHex[0] +
+      strippedHex[1] +
+      strippedHex[1] +
+      strippedHex[2] +
+      strippedHex[2];
+  }
+
+  const r = parseInt(strippedHex.substring(0, 2), 16);
+  const g = parseInt(strippedHex.substring(2, 4), 16);
+  const b = parseInt(strippedHex.substring(4, 6), 16);
+
+  return { r, g, b };
+};
+
+// 7. Convert RGB Color To Hex-Challenge Requirements
+//create the function converRGBToHex
+//take in 3 parameters - r,g, and b
+//for each (r,g,b) - create a hex pair that is two characters long
+//return hex value starting with a hashtag
+//example - r.toString(16)
+
+const convertRGBToHex = (r, g, b) => {
+  // regardless of how many characters 'r.toString(16)' returns, '0' will be added at the start & 'slice(-2)' will provide the last 2 characters of the string;
+  const firstPair = ("0" + r.toString(16)).slice(-2);
+  const secondPair = ("0" + g.toString(16)).slice(-2);
+  const thirdPair = ("0" + b.toString(16)).slice(-2);
+
+  const hex = "#" + firstPair + secondPair + thirdPair;
+  return hex;
+};
+
+console.log(convertRGBToHex(255, 255, 255));
+// #ffffff
+console.log(convertRGBToHex(0, 255, 255));
+// #00ffff
